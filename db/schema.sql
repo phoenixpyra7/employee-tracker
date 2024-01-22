edbaa492d1443fd5d33c820850a1d1919e28c99d
Seven <phoenixpyra7@gmail.com>
2024-01-22 17:34:34 -0600

    fixed syntax error in schema, removed implied not null

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE employee_db;
 
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE roles (
@@ -13,17 +13,15 @@ CREATE TABLE roles (
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     INDEX department_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    role_id INT,
+    FOREIGN KEY(role_id) REFERENCES roles(id) ON DELETE SET NULL,
     manager_id INT,
-    INDEX manager_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    FOREIGN KEY(manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
