cc7fa670e5627fc91f1079c1df13894d9cc5b812
Seven <phoenixpyra7@gmail.com>
2024-01-22 15:41:30 -0600

    added comments to seed

--- db/seed.sql
+++ db/seed.sql
@@ -7,7 +7,7 @@ VALUES
     ('Accounting'),
     ('Human Resources');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id) /* role title, yr salery, dept above-index not zero based*/
 VALUES
     ('Controller', 90000, 3),
     ('Accounts Receivable', 60000, 3),
@@ -18,7 +18,7 @@ VALUES
     ('Marketing Analyst', 85000, 2),
     ('Marketing Assistant', 80000, 2);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id) /*frst/lst name, role above-index not zero based, mngr if aplcbl*/
 VALUES
     ('Diane', 'Kaplon', 1, NULL),
     ('David', 'Douglas', 4, NULL),
